fae2af624dd06e629b223dc8948be284e9e89408
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-03 08:27:04 +0100

    example: rebuild

--- example/index.js
+++ example/index.js
@@ -1,14 +1,14 @@
 (function () {
 'use strict';
 
-var TAU = 2 * Math.PI;
+const TAU = 2 * Math.PI;
 
-var RAD = 360 / TAU;
-var DEG = TAU / 360;
+const RAD = 360 / TAU;
+const DEG = TAU / 360;
 
-var deg = function (x) { return x * RAD; };
+const deg = x => x * RAD;
 
-var rad = function (x) { return x * DEG; };
+const rad = x => x * DEG;
 
 /**
  * Helps covert from polar
@@ -19,72 +19,60 @@ var rad = function (x) { return x * DEG; };
  * @example
  * poltocar(Math.PI);
  */
-var poltocar = function (t, r) {
-  if ( t === void 0 ) t = 0;
-  if ( r === void 0 ) r = 1;
-
-  return ({
+const poltocar = (t = 0, r = 1) => ({
   x: r * Math.cos(t),
   y: r * Math.sin(t)
 });
-};
 
-var createTaxi = function (target, handler) {
-  if ( target === void 0 ) target = {};
-
+const createTaxi = (target = {}, handler) => {
   if (!('canvas' in target)) {
     throw Error('Invalid rendering context')
   }
 
-  var draw = typeof handler === 'function' ? handler : function (sx, sy, dx, dy) {
+  const draw = typeof handler === 'function' ? handler : (sx, sy, dx, dy) => {
     target.beginPath();
     target.moveTo(sx, sy);
     target.lineTo(dx, dy);
     target.stroke();
   };
 
-  var data = { x: 0, y: 0, angle: 0, trace: true };
-  var taxi = {
+  const data = { x: 0, y: 0, angle: 0, trace: true };
+  const taxi = {
     get data() {
       return Object.assign({}, data, { angle: deg(data.angle) })
     }
   };
 
-  taxi.goto = function (x, y) {
-    if ( x === void 0 ) x = data.x;
-    if ( y === void 0 ) y = data.y;
-
+  taxi.goto = (x = data.x, y = data.y) => {
     data.x = x;
     data.y = y;
 
     return taxi
   };
 
-  taxi.mask = taxi.pu = function () {
+  taxi.mask = taxi.pu = () => {
     data.trace = false;
 
     return taxi
   };
 
-  taxi.tail = taxi.pd = function () {
+  taxi.tail = taxi.pd = () => {
     data.trace = true;
 
     return taxi
   };
 
-  taxi.turn = taxi.lt = function (a) {
-    if ( a === void 0 ) a = 0;
-
+  taxi.turn = taxi.lt = (a = 0) => {
     data.angle += rad(a);
 
     return taxi
   };
 
-  taxi.move = taxi.fd = function (r) {
-    var step = poltocar(data.angle, r || 0);
+  taxi.move = taxi.fd = (r) => {
+    const step = poltocar(data.angle, r || 0);
 
-    var x = data.x + step.x;
-    var y = data.y - step.y;
+    const x = data.x + step.x;
+    const y = data.y - step.y;
 
     if (data.trace) {
       draw(data.x, data.y, x, y);
@@ -93,43 +81,52 @@ var createTaxi = function (target, handler) {
     return taxi.goto(x, y)
   };
 
-  taxi.rt = function (v) { return taxi.lt(-v); };
-  taxi.bk = function (v) { return taxi.fd(-v); };
+  taxi.rt = v => taxi.lt(-v);
+  taxi.bk = v => taxi.fd(-v);
 
   return taxi
 };
 
-var lsys = function (rules, axiom) {
-  if ( rules === void 0 ) rules = new Map();
-  if ( axiom === void 0 ) axiom = '';
+const lsys = (rules = new Map(), axiom = '') => {
+  const keys = [];
 
-  var next = '';
+  for (const k of rules.keys()) {
+    keys.push(k);
+  }
 
-  return function () {
-    next = next.length ? next.split('').map(function (v) { return rules.get(v) || v; }).join('') : axiom;
+  const match = RegExp(keys.join('|'), 'g');
+  const plant = k => rules.get(k);
+
+  let memo = axiom;
+  let next = '';
+
+  return () => {
+    // Allow for an axiom only first step
+    next = memo;
+    memo = next.replace(match, plant);
 
     return next
   }
 };
 
-var board = document.querySelector('canvas').getContext('2d');
+const board = document.querySelector('canvas').getContext('2d');
 
-var rules = new Map([
+const rules = new Map([
   ['F', 'FF-F-F-F-F-F+F']
 ]);
 
-var shape = lsys(rules, 'F-F-F-F');
-var agent = createTaxi(board).goto(380.5, 159.5);
+const shape = lsys(rules, 'F-F-F-F');
+const agent = createTaxi(board).goto(380.5, 159.5);
 
 board.strokeStyle = 'white';
 
-for (var i = 0; i < 4; i += 1) {
+for (let i = 0; i < 4; i += 1) {
   board.clearRect(0, 0, 540, 360);
 
-  var data = shape();
+  const data = shape();
 
-  for (var j = 0, stop = data.length; j < stop; j += 1) {
-    var step = data[j];
+  for (let j = 0, stop = data.length; j < stop; j += 1) {
+    const step = data[j];
 
     switch (step) {
     case 'F':
@@ -148,4 +145,3 @@ for (var i = 0; i < 4; i += 1) {
 }
 
 }());
-
