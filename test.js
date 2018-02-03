a3aaba188175125d7c93891bfd1a5322a1f865fe
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-03 08:25:54 +0100

    test: cleanup

--- test.js
+++ test.js
@@ -8,44 +8,48 @@ equal(typeof lsys()(), 'string', 'got string on call')
 equal(lsys()().length, 0)
 
 // Data from https://en.wikipedia.org/wiki/L-system
-const systems = [{
-  title: 'Algae',
-  axiom: 'A',
-  rules: new Map([
-    ['A', 'AB'],
-    ['B', 'A']
-  ]),
-  fruit: [
-    'A',
-    'AB',
-    'ABA',
-    'ABAAB'
-  ]
-}, {
-  title: 'Py tree',
-  axiom: '0',
-  rules: new Map([
-    ['1', '11'],
-    ['0', '1[0]0']
-  ]),
-  fruit: [
-    '0',
-    '1[0]0',
-    '11[1[0]0]1[0]0',
-    '1111[11[1[0]0]1[0]0]11[1[0]0]1[0]0'
-  ]
-}, {
-  title: 'Koch curve',
-  axiom: 'F',
-  rules: new Map([
-    ['F', 'F+F−F−F+F']
-  ]),
-  fruit: [
-    'F',
-    'F+F−F−F+F',
-    'F+F−F−F+F+F+F−F−F+F−F+F−F−F+F−F+F−F−F+F+F+F−F−F+F'
-  ]
-}]
+const systems = [
+  {
+    title: 'Algae',
+    axiom: 'A',
+    rules: new Map([
+      ['A', 'AB'],
+      ['B', 'A']
+    ]),
+    fruit: [
+      'A',
+      'AB',
+      'ABA',
+      'ABAAB'
+    ]
+  },
+  {
+    title: 'Py tree',
+    axiom: '0',
+    rules: new Map([
+      ['1', '11'],
+      ['0', '1[0]0']
+    ]),
+    fruit: [
+      '0',
+      '1[0]0',
+      '11[1[0]0]1[0]0',
+      '1111[11[1[0]0]1[0]0]11[1[0]0]1[0]0'
+    ]
+  },
+  {
+    title: 'Koch curve',
+    axiom: 'F',
+    rules: new Map([
+      ['F', 'F+F−F−F+F']
+    ]),
+    fruit: [
+      'F',
+      'F+F−F−F+F',
+      'F+F−F−F+F+F+F−F−F+F−F+F−F−F+F−F+F−F−F+F+F+F−F−F+F'
+    ]
+  }
+]
 
 systems.forEach(({ rules, axiom, fruit, title }) => {
   const sys = lsys(rules, axiom)
